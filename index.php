c4e3146db0982254550bc274a49dbd22b66b9ab5
Lukmannudin <32972299+Lukmannudin@users.noreply.github.com>
2019-04-05 01:02:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM User";
+            $sql_select = "SELECT * FROM [dbo].[User]";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             var_dump($registrants);
